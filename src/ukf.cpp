8c94b337d6f2165f45a9dea6141d226f0c6d8efc
Anurag Kankanala <anuragk.1005@gmail.com>
2020-05-17 19:34:53 +0530

    Init variables for UKF class

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -7,7 +7,8 @@ using Eigen::VectorXd;
 /**
  * Initializes Unscented Kalman filter
  */
-UKF::UKF() {
+UKF::UKF()
+{
   // if this is false, laser measurements will be ignored (except during init)
   use_laser_ = true;
 
@@ -25,7 +26,7 @@ UKF::UKF() {
 
   // Process noise standard deviation yaw acceleration in rad/s^2
   std_yawdd_ = 30;
-  
+
   /**
    * DO NOT MODIFY measurement noise values below.
    * These are provided by the sensor manufacturer.
@@ -45,27 +46,43 @@ UKF::UKF() {
 
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
-  
+
   /**
    * End DO NOT MODIFY section for measurement noise values 
    */
-  
+
   /**
-   * TODO: Complete the initialization. See ukf.h for other member properties.
+   * DONE: Complete the initialization. See ukf.h for other member properties.
    * Hint: one or more values initialized above might be wildly off...
    */
+  is_initialized_ = false;
+
+  n_x_ = 5;
+  x_ = VectorXd(n_x_);       //state vector
+  P_ = MatrixXd(n_x_, n_x_); //state covariance matrix
+
+  n_aug_ = 7;
+
+  lambda_ = 3 - n_x_;
+
+  //TODO change these to get a suitable NIS value
+  std_a_ = 0.5;
+
+  std_yawdd_ = 0.5;
 }
 
 UKF::~UKF() {}
 
-void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
+void UKF::ProcessMeasurement(MeasurementPackage meas_package)
+{
   /**
    * TODO: Complete this function! Make sure you switch between lidar and radar
    * measurements.
    */
 }
 
-void UKF::Prediction(double delta_t) {
+void UKF::Prediction(double delta_t)
+{
   /**
    * TODO: Complete this function! Estimate the object's location. 
    * Modify the state vector, x_. Predict sigma points, the state, 
@@ -73,7 +90,8 @@ void UKF::Prediction(double delta_t) {
    */
 }
 
-void UKF::UpdateLidar(MeasurementPackage meas_package) {
+void UKF::UpdateLidar(MeasurementPackage meas_package)
+{
   /**
    * TODO: Complete this function! Use lidar data to update the belief 
    * about the object's position. Modify the state vector, x_, and 
@@ -82,7 +100,8 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
    */
 }
 
-void UKF::UpdateRadar(MeasurementPackage meas_package) {
+void UKF::UpdateRadar(MeasurementPackage meas_package)
+{
   /**
    * TODO: Complete this function! Use radar data to update the belief 
    * about the object's position. Modify the state vector, x_, and 
